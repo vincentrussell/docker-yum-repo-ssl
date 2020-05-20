44f1dda4880feda169e4cc202b5cd4a7b02a9a9f
Vincent Russell <vincent.russell@gmail.com>
2020-05-19 22:52:22 -0400

    fix bug where docker container couldn't be restarted

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,14 @@ EXPOSE 443
 
 COPY nginx-ssl.conf /etc/nginx/nginx-ssl.conf
 COPY entrypoint.sh /root/entrypoint-extended.sh
-RUN chmod 700 /root/entrypoint-extended.sh
-RUN mkdir -p /cert
+RUN chmod 700 /root/entrypoint-extended.sh \
+ && mkdir -p /cert \
+ && yum install -y curl \
+ && yum clean all \
+ && rm -rf /var/cache/yum/*
+
+HEALTHCHECK --interval=30s --timeout=30s CMD curl --fail –key /cert/cert.key –cert /cert/cert.pem --insecure -I https://localhost:443 || exit 1
+
 VOLUME /cert/cert.pem /cert/cert.key
 ENTRYPOINT ["/root/entrypoint-extended.sh"]
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,33 @@
 #!/bin/bash
 
-if [[ -f /cert/cert.pem && -f /cert/cert.key ]]; then
-    echo "replace nginx.conf with nginx-ssl.conf"
-    rm -f /etc/nginx/nginx.conf
-    mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
+if [[ -f /cert/cert.pem && -f /cert/cert.key  ]]; then
+    if [[ -f /etc/nginx/nginx-ssl.conf ]]; then
+      echo "replace nginx.conf with nginx-ssl.conf"
+      rm -f /etc/nginx/nginx.conf
+      mv /etc/nginx/nginx-ssl.conf /etc/nginx/nginx.conf
+    fi
 else
     echo "could not find /cert/cert.pem and /cert/cert.key so using regular nginx.conf"
 fi
 
+if [[ -d "/logs/repo-scanner" ]]; then
+   echo "deleting /logs/repo-scanner"
+   rm -rf /logs/repo-scanner
+fi
+
+if [[ -d "/logs/nginx" ]]; then
+  echo "deleting /logs/nginx"
+   rm -rf /logs/nginx
+fi
+
+if [[ -d "/logs/supervisord" ]]; then
+    echo "deleting /logs/supervisord"
+   rm -rf /logs/supervisord
+fi
+
+if [[ -f "/run/supervisord.pid" ]]; then
+    echo "deleting /run/supervisord.pid"
+   rm -rf /run/supervisord.pid
+fi
 
 exec /root/entrypoint.sh
